7c0014c737be744c959ee405f9f12cf0a0760592
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-11-01 16:34:26 -0600

    added more comments

--- src/root.zig
+++ src/root.zig
@@ -23,35 +23,46 @@ pub fn LinkedList(T: type) type {
 
         /// A `Node` in the list
         pub const Node = struct {
-            // A pointer to the actual data at this node
+            /// A pointer to the actual data at this node
             data: *T,
-            // A nullable pointer to the next node in the list
+            /// A nullable pointer to the next node in the list
             next: ?*Node,
 
+            /// allocate a `Node`
             pub fn init(allocator: Allocator, value: T, next: ?*Node) Allocator.Error!*@This() {
+                // allocate memory for the node
                 const output = try allocator.create(@This());
+                // allocate memory for the value
                 output.data = try allocator.create(T);
+
+                // assign params to output
                 output.data.* = value;
                 output.next = next;
+
                 return output;
             }
             pub fn deinit(self: *@This(), allocator: Allocator) void {
+                // recursively destroy all nodes after this one
+                // `self.next` being `null` is the base case
                 if (self.next) |next| next.deinit(allocator);
+                // deallocate data and the node itself
                 allocator.destroy(self.data);
                 allocator.destroy(self);
             }
         };
 
-        /// the front of the singly linked list
+        /// the front of the singly linked list. default to `null`
         head: ?*Node = null,
-        /// the number of `Node`s in this list
+        /// the number of `Node`s in this list. default to zero
         length: usize = 0,
 
+        /// use the default values to initialize a `LinkedList`. use `defer` to clean up `Node`s
         pub const init = Self{};
         pub fn deinit(self: *Self, allocator: Allocator) void {
             if (self.head) |head| head.deinit(allocator);
         }
 
+        /// define how to loop over the linked list without exposing the underlying `Node`s of the list
         pub const Iterator = struct {
             current: ?*Node,
             pub fn next(self: *@This()) ?*T {
@@ -63,7 +74,6 @@ pub fn LinkedList(T: type) type {
         pub fn iterator(self: *Self) Iterator {
             return .{ .current = self.head };
         }
-
         pub fn get(self: *Self, i: usize) ?*T {
             if (i >= self.length) return null;
             var iter = self.iterator();
@@ -72,6 +82,7 @@ pub fn LinkedList(T: type) type {
                 if (i == n) return element;
             return null;
         }
+
         pub fn push(self: *Self, allocator: Allocator, value: T) Allocator.Error!void {
             self.head = try Node.init(allocator, value, self.head);
             self.length += 1;
