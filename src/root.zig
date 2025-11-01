2d95a7dfc9cb886ac70156ea2599e0343e67831b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-11-01 14:32:14 -0600

    imported Allocator

--- src/root.zig
+++ src/root.zig
@@ -2,8 +2,12 @@
 //!
 //! By convention, root.zig is the root source file when making a library.
 
+// IMPORTS
+
 // `const` creates an immutable binding.
 // In this case it is a type binding.
 // the specific type is the standard library type which is just module.
 // the standard library type is accessed through the compiler builtin function `@import`
-const std = @import("std");
+const std: type = @import("std");
+// create a type level binding to the standard library's `Allocator` interface.
+const Allocator: type = std.mem.Allocator;
