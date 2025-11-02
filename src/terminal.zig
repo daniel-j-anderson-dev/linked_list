ee9e3e836f34c8813c4e3b50c8835609fdd433fd
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-11-01 20:46:39 -0600

    added helpers for stack buffered stdin and stdout

---
+++ src/terminal.zig
@@ -0,0 +1,40 @@
+const builtin = @import("builtin");
+const std = @import("std");
+const File = std.fs.File;
+const Io = std.Io;
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+fn stack_buffered(comptime n: usize) type {
+    return struct {
+        const Self = @This();
+        
+        pub fn print(comptime format_string: []const u8, format_arguments: anytype) !void {
+            var stdout_buffer = [_]u8{0} ** n;
+            var stdout_reader: File.Writer = File.stdout().writer(&stdout_buffer);
+            var stdout: *Io.Writer = &stdout_reader.interface;
+
+            try stdout.print(format_string, format_arguments);
+            try stdout.flush();
+        }
+        pub fn readLine(allocator: Allocator) !ArrayList(u8) {
+            var stdin_buffer = [_]u8{0} ** n;
+            var stdin_reader: File.Reader = File.stdin().reader(&stdin_buffer);
+            var stdin: *Io.Reader = &stdin_reader.interface;
+
+            var line = Io.Writer.Allocating.init(allocator);
+            defer line.deinit();
+            _ = try stdin.streamDelimiterEnding(&line.writer, '\n');
+            return line.toArrayList();
+        }
+        fn input(allocator: Allocator, comptime format_string: []const u8, format_arguments: anytype) !ArrayList(u8) {
+            try Self.print(format_string, format_arguments);
+            return try Self.readLine(allocator);
+        }
+    };
+}
+
+const buffer_size = 1024;
+pub const input = stack_buffered(buffer_size).input;
+pub const print = stack_buffered(buffer_size).print;
+pub const readLine = stack_buffered(buffer_size).readLine;
