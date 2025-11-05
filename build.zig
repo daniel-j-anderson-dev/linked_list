5fc4b1c820a882c20329b49e101f1b40b19371f7
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-11-05 15:46:09 -0700

    customized build script

--- build.zig
+++ build.zig
@@ -1,157 +1,88 @@
 const std = @import("std");
+const OptimizeMode = std.builtin.OptimizeMode;
+const Build = std.Build;
 
-// Although this function looks imperative, it does not perform the build
-// directly and instead it mutates the build graph (`b`) that will be then
-// executed by an external runner. The functions in `std.Build` implement a DSL
-// for defining build steps and express dependencies between them, allowing the
-// build runner to parallelize the build automatically (and the cache system to
-// know when a step doesn't need to be re-run).
-pub fn build(build_graph: *std.Build) void {
-    // Standard target options allow the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+const executable_name = "a.out";
+const executable_path = "src/main.zig";
+
+const str = []const u8; // type alias `str` for a slice of bytes
+const module_meta_data = [_]struct { str, str }{
+    .{ "linked_list", "src/root.zig" },
+    .{ "terminal", "src/terminal.zig" },
+};
+const module_count = module_meta_data.len;
+
+pub fn build(build_graph: *Build) void {
+    // SETTINGS
+
+    // use zig to choose a default build target
     const target = build_graph.standardTargetOptions(.{});
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimizationOptions = build_graph.standardOptimizeOption(.{});
-    // It's also possible to define more custom flags to toggle optional features
-    // of this build script using `b.option()`. All defined flags (including
-    // target and optimize options) will be listed when running `zig build --help`
-    // in this directory.
+    // use zig to choose a default build optimization
+    const optimization_options = build_graph.standardOptimizeOption(.{});
 
-    // This creates a module, which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Zig modules are the preferred way of making Zig code available to consumers.
-    // addModule defines a module that we intend to make available for importing
-    // to our consumers. We must give it a name because a Zig package can expose
-    // multiple modules and consumers will need to be able to specify which
-    // module they want to access.
-    const library_module = build_graph.addModule("linked_list", .{ // 😀
-        // The root source file is the "entry point" of this module. Users of   
-        // this module will only be able to access public declarations contained
-        // in this file, which means that if you have declarations that you
-        // intend to expose to consumers that were defined in other files part
-        // of this module, you will have to make sure to re-export them from
-        // the root file.
-        .root_source_file = build_graph.path("src/root.zig"), // 😀
-        // Later on we'll use this module as the root module of a test executable
-        // which requires us to specify a target.
-        .target = target,
-    });
+    // MODULES
 
-    // Here we define an executable. An executable needs to have a root module
-    // which needs to expose a `main` function. While we could add a main function
-    // to the module defined above, it's sometimes preferable to split business
-    // business logic and the CLI into two separate modules.
-    //
-    // If your goal is to create a Zig library for others to use, consider if
-    // it might benefit from also exposing a CLI tool. A parser library for a
-    // data serialization format could also bundle a CLI syntax checker, for example.
-    //
-    // If instead your goal is to create an executable, consider if users might
-    // be interested in also being able to embed the core functionality of your
-    // program in their own executable in order to avoid the overhead involved in
-    // subprocessing your CLI tool.
-    //
-    // If neither case applies to you, feel free to delete the declaration you
-    // don't need and to put everything under a single module.
-    const executable = build_graph.addExecutable(.{ // 😀 configure the executable
-        .name = "linked_list",
-        .root_module = build_graph.createModule(.{
-            // b.createModule defines a new module just like b.addModule but,
-            // unlike b.addModule, it does not expose the module to consumers of
-            // this package, which is why in this case we don't have to give it a name.
-            .root_source_file = build_graph.path("src/main.zig"),
-            // Target and optimization levels must be explicitly wired in when
-            // defining an executable or library (in the root module), and you
-            // can also hardcode a specific target for an executable or library
-            // definition if desireable (e.g. firmware for embedded devices).
-            .target = target,
-            .optimize = optimizationOptions,
-            // List of modules available for import in source files part of the
-            // root module.
-            .imports = &.{ // 😀 allow `@import("link_list")`
-                // Here "linked_list" is the name you will use in your source code to
-                // import this module (e.g. `@import("linked_list")`). The name is
-                // repeated because you are allowed to rename your imports, which
-                // can be extremely useful in case of collisions (which can happen
-                // importing modules from different packages).
-                .{ .name = "    linked_list", .module = library_module },
+    // declare arrays to hold pointers to each `Build.Module` added to the `build_graph` and
+    var modules: [module_count]*Build.Module = undefined;
+    var executable_imports: [module_count]Build.Module.Import = undefined;
+    // populate arrays
+    for (0..module_count) |i| {
+        // get the name and path of each module from the global constant
+        const name, const path = module_meta_data[i];
+        // add a node that represent the `i`th source file as a module
+        modules[i] = build_graph.addModule(
+            name,
+            .{
+                .root_source_file = build_graph.path(path),
+                .target = target,
+                .optimize = optimization_options,
             },
-        }),
-    });
-
-    // This declares intent for the executable to be installed into the
-    // install prefix when running `zig build` (i.e. when executing the default
-    // step). By default the install prefix is `zig-out/` but can be overridden
-    // by passing `--prefix` or `-p`.
-    build_graph.installArtifact(executable);
-
-    // This creates a top level step. Top level steps have a name and can be
-    // invoked by name when running `zig build` (e.g. `zig build run`).
-    // This will evaluate the `run` step rather than the default step.
-    // For a top level step to actually do something, it must depend on other
-    // steps (e.g. a Run step, as we will see in a moment).
-    const run_step = build_graph.step("run", "Run the app");
-
-    // This creates a RunArtifact step in the build graph. A RunArtifact step
-    // invokes an executable compiled by Zig. Steps will only be executed by the
-    // runner if invoked directly by the user (in the case of top level steps)
-    // or if another step depends on it, so it's up to you to define when and
-    // how this Run step will be executed. In our case we want to run it when
-    // the user runs `zig build run`, so we create a dependency link.
-    const run_command = build_graph.addRunArtifact(executable);
-    run_step.dependOn(&run_command.step);
-
-    // By making the run step depend on the default step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    run_command.step.dependOn(build_graph.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (build_graph.args) |args| {
-        run_command.addArgs(args);
+        );
+        executable_imports[i] = .{
+            .name = name,
+            .module = modules[i],
+        };
     }
 
-    // Creates an executable that will run `test` blocks from the provided module.
-    // Here `mod` needs to define a target, which is why earlier we made sure to
-    // set the releative field.
-    const library_tests = build_graph.addTest(.{
-        .root_module = library_module,
+    // EXECUTABLES
+
+    // add a node to compile the executable
+    const compile_executable = build_graph.addExecutable(.{
+        .name = executable_name,
+        .root_module = build_graph.createModule(.{
+            .root_source_file = build_graph.path(executable_path),
+            .target = target,
+            .optimize = optimization_options,
+            .imports = &executable_imports,
+        }),
     });
+    // add a node to store the compiled executable binary as an artifact
+    build_graph.installArtifact(compile_executable);
 
-    // A run step that will run the test executable.
-    const run_library_tests = build_graph.addRunArtifact(library_tests);
+    // COMMANDS
 
-    // Creates an executable that will run `test` blocks from the executable's
-    // root module. Note that test executables only test one module at a time,
-    // hence why we have to create two separate ones.
-    const executable_tests = build_graph.addTest(.{
-        .root_module = executable.root_module,
-    });
+    // declare `zig build run`
+    const run_command = build_graph.step("run", "Run the app");
+    // add a node to run the compiled executable. (`compile_executable` is the artifact that will be ran)
+    const run_executable = build_graph.addRunArtifact(compile_executable);
+    // running the executable depends on artifacts being installed
+    run_executable.step.dependOn(build_graph.getInstallStep());
+    // pass arguments from the build graph into the run command
+    if (build_graph.args) |args| run_executable.addArgs(args);
+    run_command.dependOn(&run_executable.step);
 
-    // A run step that will run the second test executable.
-    const run_executable_tests = build_graph.addRunArtifact(executable_tests);
-
-    // A top level step for running all tests. dependOn can be called multiple
-    // times and since the two run steps do not depend on one another, this will
-    // make the two of them run in parallel.
-    const test_step = build_graph.step("test", "Run tests");
-    test_step.dependOn(&run_library_tests.step);
-    test_step.dependOn(&run_executable_tests.step);
-
-    // Just like flags, top level steps are also listed in the `--help` menu.
-    //
-    // The Zig build system is entirely implemented in userland, which means
-    // that it cannot hook into private compiler APIs. All compilation work
-    // orchestrated by the build system will result in other Zig compiler
-    // subcommands being invoked with the right flags defined. You can observe
-    // these invocations when one fails (or you pass a flag to increase
-    // verbosity) to validate assumptions and diagnose problems.
-    //
-    // Lastly, the Zig build system is relatively simple and self-contained,
-    // and reading its source code will allow you to master it.
+    // declare `zig build run`
+    const test_command = build_graph.step("test", "Run tests");
+    // add each test node
+    for (modules) |module| {
+        // add a compilation node for the test module
+        const compile_module_tests = build_graph.addTest(.{ .root_module = module });
+        // Add a run node for the module's tests
+        const run_module_tests = build_graph.addRunArtifact(compile_module_tests);
+        // the test command depends on each module's tests
+        test_command.dependOn(&run_module_tests.step);
+    }
+    const compile_executable_test = build_graph.addTest(.{ .root_module = compile_executable.root_module });
+    const run_module_tests = build_graph.addRunArtifact(compile_executable_test);
+    test_command.dependOn(&run_module_tests.step);
 }
-
