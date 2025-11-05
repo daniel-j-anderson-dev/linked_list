868e22b50f18ffc21343749bc4e947eab93631f3
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-11-05 16:08:04 -0700

    added simple example of push and pop

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,29 @@
 const std = @import("std");
-const DefaultGeneralPurposeAllocator = std.heap.GeneralPurposeAllocator(.{});
+const ArrayList = std.ArrayList;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
+const Allocator = std.mem.Allocator;
 
-/// this is a binding to the type described in `root.zig`.
-/// - this is where the type is defined in the build graph see: `build.zig` line31 col43 and line38 col47
-/// - this is where the type made visible to `main.zig` in the build graph see: `build.zig` line60 col36 and line81 col29
+/// this is a binding to the type described in `linked_list.zig`. see `build.zig` line 10
 const linked_list = @import("linked_list");
-/// this is a binding to the type described in `terminal.zig` by path instead of manually thru the build graph
-const terminal = @import("terminal.zig");
+const LinkedList = linked_list.LinkedList;
+
+/// this is a binding to the type described in `terminal.zig`. see `build.zig` line 11
+const terminal = @import("terminal");
+
+const String = ArrayList(u8);
 
 pub fn main() !void {
-    var general_purpose_allocator = DefaultGeneralPurposeAllocator.init;
+    var general_purpose_allocator = GeneralPurposeAllocator(.{}).init;
     const allocator = general_purpose_allocator.allocator();
 
-    var x = try terminal.input(allocator, "enter a value for x: ", .{});
-    defer x.deinit(allocator);
-    try terminal.print("{s}\n", .{x.items});
+    var list = LinkedList(String).empty;
+    for (0..5) |i| {
+        const input = try terminal.input(allocator, "enter {}: ", .{i});
+        try list.push(allocator, input);
+    }
+    while (list.pop()) |popped_node|
+        try terminal.print(
+            "{s}\n",
+            .{popped_node.data.items},
+        );
 }
